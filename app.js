e213cc64dc3e585ae7b06a5453a704b187de13c3
celiajrl <jimenezrealcelia@gmail.com>
2024-04-17 22:59:22 +0200

    last

--- app.js
+++ app.js
@@ -85,18 +85,15 @@ app.get('/:activeId', async (req, res) => {
             });
 
             console.log('All files copied. Initializing Rasa server...');
-            const spawnOptions = {
-                cwd: extractPath,
-                shell: true,
-                detached: true,
-                stdio: 'inherit',
-                env: process.env,
-                timeout: 1200000 // Establece un timeout más largo, por ejemplo 20 minutos
-            };
-            const rasaRun = spawn('/app/venv/bin/rasa', ['run', '--enable-api', '--cors', '*', '--port', '5005'], spawnOptions);
+        
+            const rasaRun = spawn('/app/venv/bin/rasa', ['run', '--enable-api', '--cors', '*', '--port', '5005'], {cwd: extractPath});
 
             rasaRun.stdout.on('data', (data) => {
                 console.log(`Rasa Run STDOUT: ${data.toString()}`);
+                if (data.toString().includes("Rasa server is up and running")) {
+                    console.log('Rasa server confirmed up and running.');
+                    res.sendFile(path.join(__dirname, 'index.html'));
+                }
             });
 
             rasaRun.stderr.on('data', (data) => {
