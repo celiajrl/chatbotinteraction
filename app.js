0f0fd27111575b03d4d8dc97a34c31590d81a156
celiajrl <jimenezrealcelia@gmail.com>
2024-04-17 23:45:31 +0200

    last

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000; 
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+app.use(cors());
 
 // Conexión a la base de datos
 let db;
